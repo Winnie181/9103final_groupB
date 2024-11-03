737fb9f9536884037052b78a111c97475d961878
Winnie181 <wxie0333@uni.sydney.edu.au>
2024-11-03 12:55:57 +0800

    final

--- final project/sketch.js
+++ final project/sketch.js
@@ -1,34 +1,32 @@
 let myCircles = [];
 let numOfCircles = 33;
 let bgImage
-let newImage
-let bottomX = 85;  // 第一个半圆的 X 位置
-let bottomY = 595;      // 半圆的 Y 位置
-let diameter1 = 45; // 每个半圆的直径
+let bottomX = 110;  // 第一个半圆的 X 位置
+let bottomY = 495;      // 半圆的 Y 位置
+let diameter1 = 35; // 每个半圆的直径
 let diameter2 = 25; // 每个半圆的直径
-let spacing1 = 49;  // 半圆之间的间距
-let spacing2 = 145
-let spacing3 = 75
-let topX1 = 135
-let topX2 = 170
-let topY = 520
+let spacing1 = 37;  // 半圆之间的间距
+let spacing2 = 105
+let spacing3 = 43
+let topX1 = 148
+let topX2 = 180
+let topY = 445
 function preload() {
    // 预加载背景图片
-   //bgImage = loadImage('assets/background.jpg')
-   newImage = loadImage('assets/1.jpeg')
+   bgImage = loadImage('assets/background.jpg')
 }
 
 let circlePositions =[[85,40],[85,85],[90,120],[114,130],[122,153],[120,183],[125,224],[150,248],[175,252],[198,247],
 [222,253],[247,250],[272,248],[280,218],[285,190],[289,158],[285,125],[300,120],[325,125],[350,134],[358,115],
 [180,165],[170,185],[190,183],[210,204],[230,185],[241,170],[210,230],
-[210,289],[200,340],[202,385],[208,410],[195,430]
+[210,289],[200,340],[202,385],[208,410],[200,433]
 ];
 
 
 let circleDiameters = [50, 43, 29, 27, 23, 40, 53, 28, 26, 20,
 31, 22, 33, 35, 25, 44, 20, 15, 33, 22, 20,
 16, 16, 26, 35, 22, 16, 20,
-47, 61, 30, 23, 35];
+47, 61, 30, 23, 25];
 
 function setup(){
    createCanvas(400,600);
@@ -46,6 +44,7 @@ function setup(){
      this.speed = 2;
      this.color1 = color(228, 102, 103);
      this.color2 = color(142, 171, 126);
+     this.stroke = color(0)
      this.originalX = x;
      this.originalY = y;
      this.isMoving = false;
@@ -54,6 +53,7 @@ function setup(){
   
     draw(){
      fill(this.color1);
+     stroke(this.stroke)
      arc(this.x,this.y,this.size,this.size, HALF_PI, -HALF_PI, PIE);
      fill(this.color2);
      arc(this.x,this.y,this.size,this.size, -HALF_PI, HALF_PI, PIE);
@@ -67,7 +67,7 @@ function setup(){
    }
   
 function draw() {
-  background(newImage);
+  background(bgImage);
 
     for (let i = 0; i < numOfCircles; i++){
       myCircles[i].draw()
@@ -76,30 +76,31 @@ function draw() {
     stroke(0)
     strokeWeight(2);
   
+     //底部绿色矩形
     fill(142, 171, 126);
-    rect(0, 530, width, 80)
-    line(30, 530, 30, 610)
-    line(390, 530, 390, 610)
-    fill(217, 194, 125)
-    rect(62, 520, 270, 75)
-  
+    rect(27, 450, 345, 55,);
+ 
+    //两边的竖线
+    line(65, 450, 65, 506);
+    line(340, 450, 340, 506);
+    //底部黄色较大矩形
+    fill(217, 194, 125);
+    rect(92, 446, 204, 50);
+    //底部红色小矩形
     fill(228, 102, 103);
-    rect(112, 520, 48, 75)
+    rect(130,446,35,50);
+    //底部绿色小矩形
     fill(142, 171, 126);
-    rect(160, 520, 48, 75)
-    fill(217, 194, 125)
-    rect(208, 520, 48, 75)
-    fill(142, 171, 126);
-    rect(256, 520, 48, 75)
-    fill(217, 194, 125)
-    rect(304, 520, 28, 75)
+    rect(165, 446, 37, 50)
+    rect(237, 446, 35, 50)
 
-  
-    arc(319,595,27,35,PI,0,fill(228, 102, 103))
+    stroke(217, 194, 125)
+    strokeWeight(2);
+    arc(285,495,20,35,PI,0,fill(228, 102, 103))
 
     for (let i = 0; i < 2; i++) {
       fill(i % 2 === 0 ? color(142, 171, 126) : color(228, 102, 103));
-      arc(topX1 + i * spacing2, topY, diameter1, diameter1, 0, PI);
+      arc(topX1 + i * spacing2, topY, diameter1, diameter1, 0, PI,);
     }
     for (let i = 0; i < 5; i++) {
         fill(i % 3 === 0 ? color(142, 171, 126) : (i % 3 === 1 ? color(217, 194, 125) : color(228, 102, 103)));
@@ -117,6 +118,8 @@ function draw() {
         fill(i % 2 === 0 ? color(142, 171, 126) : color(228, 102, 103));
         arc(topX2 + i * spacing3, topY, diameter2, diameter2, PI, 0);
     }
+    line(130, 446, 270, 446);
+
 }
 
 
